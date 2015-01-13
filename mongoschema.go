6d5f7fa707a4f0f9763b030c0dd756f09d2b338c
Naitik Shah <n@daaku.org>
2015-01-12 16:54:44 -0800

    update mgo path

--- mongoschema.go
+++ mongoschema.go
@@ -12,8 +12,8 @@ import (
 	"time"
 	"unicode"
 
-	"labix.org/v2/mgo"
-	"labix.org/v2/mgo/bson"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var errEmptyURL = errors.New("mongoschema: no URL specificed")
